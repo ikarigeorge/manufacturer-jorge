2c0235d3ded595627be2635660f308ee580e4e94
Jorge Avante Reyes <jorge.avante-reyes@kaufland.com>
2016-11-24 15:12:38 +0100

    initial commit

---
+++ src/main/java/com/kaufland/ManufacturerJorgeApplication.java
@@ -0,0 +1,12 @@
+package com.kaufland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManufacturerJorgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManufacturerJorgeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kaufland/ManufacturerJorgeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kaufland;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ManufacturerJorgeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
